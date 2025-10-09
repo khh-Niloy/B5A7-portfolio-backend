6464baa483e6711c8306ceb886218f183797a67f
khh-Niloy <khhniloy0@gmail.com>
2025-10-10 00:45:10 +0600

    updated

--- src/app/utils/cookiesService.ts
+++ src/app/utils/cookiesService.ts
@@ -1,24 +1,33 @@
 import { Response } from "express";
 
-const setCookie = (res: Response, accessToken: string, refreshToken: string)=>{
-    res.cookie("accessToken", accessToken, {
-        httpOnly: true,
-        secure: true,
-        sameSite: "none",
-    });
-    res.cookie("refreshToken", refreshToken, {
-        httpOnly: true,
-        secure: true,
-        sameSite: "none",
-    });
-}
+const setCookie = (res: Response, accessToken: string, refreshToken: string) => {
+  const cookieOptions = {
+    httpOnly: true,
+    secure: true,           
+    sameSite: "none" as const, 
+    path: "/",
+  };
 
-const clearCookie = (res: Response)=>{
-    res.clearCookie("accessToken");
-    res.clearCookie("refreshToken");
-}
+  res.cookie("accessToken", accessToken, cookieOptions);
+  res.cookie("refreshToken", refreshToken, cookieOptions);
+};
+
+const clearCookie = (res: Response) => {
+  res.clearCookie("accessToken", {
+    httpOnly: true,
+    secure: true,
+    sameSite: "none",
+    path: "/",
+  });
+  res.clearCookie("refreshToken", {
+    httpOnly: true,
+    secure: true,
+    sameSite: "none",
+    path: "/",
+  });
+};
 
 export const cookiesService = {
-    setCookie,
-    clearCookie
-}
+  setCookie,
+  clearCookie,
+};
