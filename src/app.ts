dfcdb8f95b69b0015239203fd448b37cfebfb63c
khh-Niloy <khhniloy0@gmail.com>
2025-10-10 23:28:54 +0600

    updated cookies service

--- src/app.ts
+++ src/app.ts
@@ -9,7 +9,7 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(
   cors({
-    origin: envVars.FRONTEND_URLS,
+    origin: "https://b5a7-portfolio.vercel.app",
     credentials: true,
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS'],
     allowedHeaders: ['Content-Type', 'Authorization', 'Cookie'],
--- src/app/utils/cookiesService.ts
+++ src/app/utils/cookiesService.ts
@@ -1,33 +1,32 @@
 import { Response } from "express";
 
-const setCookie = (res: Response, accessToken: string, refreshToken: string) => {
-  const cookieOptions = {
-    httpOnly: true,
-    secure: true,           
-    sameSite: "none" as const, 
-    path: "/",
-  };
+const setCookie = (res: Response, accessToken: string, refreshToken: string)=>{
+    res.cookie("accessToken", accessToken, {
+        httpOnly: true,
+        secure: true,
+        sameSite: "none",
+    });
+    res.cookie("refreshToken", refreshToken, {
+        httpOnly: true,
+        secure: true,
+        sameSite: "none",
+    });
+}
 
-  res.cookie("accessToken", accessToken, cookieOptions);
-  res.cookie("refreshToken", refreshToken, cookieOptions);
-};
-
-const clearCookie = (res: Response) => {
-  res.clearCookie("accessToken", {
-    httpOnly: true,
-    secure: true,
-    sameSite: "none",
-    path: "/",
-  });
-  res.clearCookie("refreshToken", {
-    httpOnly: true,
-    secure: true,
-    sameSite: "none",
-    path: "/",
-  });
-};
+const clearCookie = (res: Response)=>{
+    res.clearCookie("accessToken", {
+        httpOnly: true,
+        secure: true,
+        sameSite: "none",
+    });
+    res.clearCookie("refreshToken", {
+        httpOnly: true,
+        secure: true,
+        sameSite: "none",
+    });
+}
 
 export const cookiesService = {
-  setCookie,
-  clearCookie,
-};
+    setCookie,
+    clearCookie
+}
